db27070309ad17c1e1aecc7320fb838c8e45f31b
Anastasiya Brodskaya <anastasia.brodskaja@yandex.ru>
2023-09-07 14:16:52 +0300

    created new function and to updated docstring for first function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def capitalize_all_letters(string):
+    """function to update"""
     return string.upper()
+
+
+def capitalize_first_letters(string):
+    """function does capitalize
+    first letter """
+    return string.title()
